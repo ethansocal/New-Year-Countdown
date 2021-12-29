f17dd9b81a967a3b4162450ffd8848744f7f75e0
Ethan Henry <79533577+ethansocal@users.noreply.github.com>
2021-12-29 11:36:07 -0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ["./public/**/*.{html,js}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                "bungee": ["Bungee", "cursive"]
+            }
+        },
+    },
+    plugins: [],
+}
