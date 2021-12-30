00ea520d10784d2aaa19c94fa6d7103681d2fd4f
Ethan Henry <79533577+ethansocal@users.noreply.github.com>
2021-12-30 14:09:56 -0800

    add radix argument to parseInt

--- js/debug.js
+++ js/debug.js
@@ -1,8 +1,8 @@
 document.addEventListener("keydown", (e) => {
   if (e.key === "r") {
     refresh();
-  } else if (!isNaN(parseInt(e.key))) {
-    activatePart(parseInt(e.key));
+  } else if (!isNaN(parseInt(e.key, 10))) {
+    activatePart(parseInt(e.key, 10));
     override = true;
   } else if (e.key === "f") {
     fireworks = !fireworks;
