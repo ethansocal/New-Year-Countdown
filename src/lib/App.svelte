d806168e205ac3ec3d728bb9c35d1cd618d0743b
Ethan Henry <ehtanhnery@gmail.com>
2022-12-26 15:37:40 -0800

    syncing

--- src/lib/App.svelte
+++ src/lib/App.svelte
@@ -5,34 +5,15 @@
     let time = Temporal.Now.plainDateTimeISO();
     let outOfDate = false;
 
-    setTimeout(
-        () =>
-            setInterval(() => {
-                time = Temporal.Now.plainDateTimeISO();
-            }, 1000),
-        1000 - time.millisecond
-    );
-    let time2 = Temporal.Now.plainDateTimeISO();
-
     setInterval(() => {
-        time2 = Temporal.Now.plainDateTimeISO();
-    }, 1000);
-
-    let time3 = Temporal.Now.plainDateTimeISO();
-
-    setInterval(() => {
-        time3 = Temporal.Now.plainDateTimeISO();
-    }, 1);
+        time = Temporal.Now.plainDateTimeISO();
+        console.log(time.millisecond);
+    }, 100);
 </script>
 
 <main>
     <Time {time} />
-    <Time time={time2} />
-    <Time time={time3} />
-    <h1
-        class={"text-yellow-500 font-bold text-2xl" +
-            (outOfDate ? "" : " hidden")}
-    >
+    <h1 class={"text-yellow-500 font-bold text-2xl"}>
         Looks like your time is out of date!
     </h1>
 </main>
